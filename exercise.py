fa7949d60e6798bb7017ca2e58face5cc1afd2ec
teng101 <teng10.work@gmail.com>
2023-03-23 23:21:52 +0530

    edited

--- exercise.py
+++ exercise.py
@@ -15,3 +15,32 @@ print(z)
 a = 2 # scalar
 z1 = a * x1 # broadcasting
 print(z1)
+
+#Identity matrix
+identity_mat = np.eye(3) #3*3 identity matrix
+print(identity_mat)
+
+
+#zero matrix
+zero_mat = np.zeros((2,3))
+print(zero_mat)
+
+
+#ones matrix
+ones_mat = np.ones((2,3))
+print(ones_mat)
+
+
+#diagonal matrix with diagonal elements [1,2,3]
+diagonal_mat = np.diag([1,2,3])
+print(diagonal_mat)
+
+#upper triangular matrix
+up_tri_mat = np.triu(np.ones((3,3)), k=0)
+print(up_tri_mat)
+
+# create a 3x3 lower triangular matrix with 2s on the diagonal
+arr = np.tril(np.full((3, 3), 2), k=0)
+print(arr)
+
+
