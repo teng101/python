a2c7cd095dfd4439dace20a4fe7640f93df572eb
teng101 <teng10.work@gmail.com>
2023-03-25 17:06:07 +0530

    list comprehension

---
+++ exercise2.py
@@ -0,0 +1,11 @@
+#take integers as input in a list
+my_list = list(map(int, input().split(",")))
+print(my_list)
+
+#take any number as input
+my_list = list(map(float, input().split(',')))
+print(my_list)
+
+#take string as input in a list
+my_string = list(map(str,input().strip().split(" ")))
+print(my_string)
