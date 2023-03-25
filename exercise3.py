3c72969f15ae986a731f2cc335c327dfdebba64f
teng101 <teng10.work@gmail.com>
2023-03-25 19:44:43 +0530

    list comprehension

--- exercise3.py
+++ exercise3.py
@@ -2,8 +2,9 @@
 shopping_list = list()
 
 while True:
-    i = input("Add strings to the list:")
-    shopping_list.append(i)
+    i = input("Add to the shopping list:")
+    i = i.split(",")
+    shopping_list += i
     a = input("Do you wish to exit?(enter y to exit)")
     if a == "y":
         break
