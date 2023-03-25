dc596b060459d6da9e9e8d39bc6bdabb371c040e
teng101 <teng10.work@gmail.com>
2023-03-25 20:37:08 +0530

    shopping list

--- exercise3.py
+++ exercise3.py
@@ -11,11 +11,15 @@ while True:
 print(shopping_list)
 
 while True:
+    n = 0
     remove = input("Remove item:")
     remove = remove.split(",")
     for i in remove:
         if i not in shopping_list:
+            n = 1
             break
         else:
             shopping_list.remove(i)
+    if n == 1:
+        break
     print(shopping_list)
