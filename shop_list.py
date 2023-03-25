9b8a231fe9e5869f05cdb31b9c8f304f1a865534
teng101 <teng10.work@gmail.com>
2023-03-25 22:23:08 +0530

    shopping list

--- shop_list.py
+++ shop_list.py
@@ -2,21 +2,23 @@
 my_dict = dict()
 
 while True:
-    item = input("Add an item:")
+    item = input("Add an item/y to exit:")
+    if item == "y":
+        break
     amt = input("Add an amount:")
     my_dict[item] = amt
-    exit = input("Enter y to exit")
-    if exit == "y":
-        break
-    
+del my_dict[""]
+        
 print(f"Your list: {my_dict}")
 
+
 while True:
-    del_item = input("Delete item:")
-    del my_dict[del_item]
-    del my_dict[""]
-    exit = input("Enter y to exit")
-    if exit == "y":
+    del_item = input("Delete item/y to exit:")
+    if del_item == "y":
         break
+    elif del_item not in my_dict:
+        print("Item not on the list")
+        continue
+    del my_dict[del_item]
 
 print(f"Your list: {my_dict}")
