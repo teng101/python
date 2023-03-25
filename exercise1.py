5b9f50376e0d670dc9fa1bf47b78f75f5873900f
teng101 <teng10.work@gmail.com>
2023-03-25 16:35:24 +0530

    list comprehension

--- exercise1.py
+++ exercise1.py
@@ -14,10 +14,11 @@ another_string = " ".join(my_list)
 
 print(another_string)
 """
-my_list = list(new_string.split())
+my_list = list(new_string.split())# create a list out of string parsing on white space
 print(my_list)
 
-new_list = [len(i) for i in my_list]
+new_list = [len(i) for i in my_list]# counting the length of each element of the list
 print(new_list)
 
-    
+new_list.reverse()# reversing the list
+print(f"Printed in reverse: {new_list}")
